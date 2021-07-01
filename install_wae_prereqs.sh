087de714357100e6f60625b122e7645fea4f93c8
Mike Saur <mike.saur@ccisystems.com>
2021-07-01 07:50:06 -0500

    prereq update

--- install_wae_prereqs.sh
+++ install_wae_prereqs.sh
@@ -3,9 +3,9 @@ sudo yum -y install epel-release
 sudo yum update
 sudo yum -y install java-11-openjdk-devel
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-2.el8_4.x86_64/bin/java
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-2.el8_4.x86_64/bin/java
-sudo echo "export JRE_HOME=/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-2.el8_4.x86_64" >> .bash_profile
-sudo echo "export JRE_HOME=/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-2.el8_4.x86_64" >> .bash_profile
+export JRE_HOME=/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-2.el8_4.x86_64
+sudo echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-2.el8_4.x86_64/bin/java" >> .bash_profile
+sudo echo "export JRE_HOME=/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-2.el8_4" >> .bash_profile
 env | grep java
 java -version
 sudo yum -y install perl
